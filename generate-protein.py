7d21bb881b20894ecb1f79ed22674f9885450d4e
Yan <astromid@gmail.com>
2016-09-19 21:36:15 +0300

    blank for task1 & generate_dna

---
+++ generate-protein.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 19 21:15:00 2016
+
+@author: astro
+
+Generator for DNA code
+"""
+
+import numpy as np
+
+alphabet = ['C', 'G', 'A', 'T']
+N = 10000
+data = ''
+for i in range(0, N):
+    index = np.random.choice(len(alphabet))
+    data += alphabet[index]
+
+output = open('dna.txt', 'w')
+output.write(data)
+output.close()
---
+++ task1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 19 21:12:31 2016
+
+@author: astro
+
+Task 1 - compare Knuth–Morris–Pratt and bruteforce algorithms
+"""
+
+
+
