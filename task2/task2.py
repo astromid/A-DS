41ee77ac552ac594c4619d6209d934cf25b8d442
Yan <astromid@gmail.com>
2016-10-17 16:13:04 +0300

    task2 fixed

--- task2/task2.py
+++ task2/task2.py
@@ -9,56 +9,61 @@ import numpy as np
 
 
 def generateRandomArr(n):
-    arr = [np.random.choice(1000) for i in range(0, n)]
-    return arr, arr
+    arr = [np.random.choice(10000) for i in range(0, n)]
+    return arr
 
 
 def pasteSort(arr):
     n = len(arr)
-    N = 0
+    N_cmp = 0
+    N_swap = 0
     for i in range(1, n):
         x = arr[i]
         j = i
-        N += 1
+        N_cmp += 1
         while j > 0 and arr[j-1] > x:
-            N += 2
+            N_cmp += 1
+            N_swap += 1
             arr[j] = arr[j-1]
             j -= 1
         arr[j] = x
-    return N
+    return (N_cmp, N_swap)
 
 
 def shakeSort(arr):
     l = 0
     r = len(arr) - 1
-    N = 0
+    N_cmp = 0
+    N_swap = 0
     while l <= r:
         for i in range(l, r):
-            N += 1
+            N_cmp += 1
             if arr[i] > arr[i+1]:
-                N += 2
+                N_swap += 2
                 arr[i], arr[i+1] = arr[i+1], arr[i]
         r -= 1
         for i in range(r, l, -1):
-            N += 1
+            N_cmp += 1
             if arr[i-1] > arr[i]:
-                N += 2
+                N_swap += 2
                 arr[i], arr[i-1] = arr[i-1], arr[i]
         l += 1
-    return N
+    return (N_cmp, N_swap)
 
 '''
-for length in [1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000]:
-    lst1, lst2 = generateRandomArr(length)
-    N1 = pasteSort(lst1)
-    N2 = shakeSort(lst2)
-    print('Len = {}, paste = {}, shaker = {}\n'.format(length, N1, N2))
+tlen = [1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 15000, 20000, 25000, 30000]
+for length in tlen:
+    lst = generateRandomArr(length)
+    N1_cmp, N1_swap = pasteSort(lst.copy())
+    N2_cmp, N2_swap = shakeSort(lst.copy())
+    msg = 'Len = {}, paste: cmp {}, swap {}; shaker: cmp {}, swap {}\n'
+    print(msg.format(length, N1_cmp, N1_swap, N2_cmp, N2_swap))
 '''
 
 
 def oneStepBubble(arr, direction):
     n = len(arr)
-    # true - >, false - <
+    # direction: bool, true - >, false - <
     if direction:
         for i in range(n-1):
             if arr[i] > arr[i+1]:
@@ -69,20 +74,23 @@ def oneStepBubble(arr, direction):
                 arr[i], arr[i+1] = arr[i+1], arr[i]
 
 
-lst1, lst2 = generateRandomArr(50)
+lst = generateRandomArr(1000)
 data = []
-data.append(lst1.copy())
-for i in range(50):
-    oneStepBubble(lst1, True)
-    data.append(lst1.copy())
-for i in range(50):
+data.append(lst.copy())
+lst2 = lst.copy()
+for i in range(1000):
+    oneStepBubble(lst, True)
+    data.append(lst.copy())
+for i in range(1000):
     oneStepBubble(lst2, False)
     data.insert(0, lst2.copy())
 
-for i in range(len(data)):
+# с шагом в 0,02 по sort ratio
+for i in range(0, len(data), 20):
     data_curr1 = data[i].copy()
     data_curr2 = data[i].copy()
-    N1 = pasteSort(data_curr1)
-    N2 = shakeSort(data_curr2)
-    ratio = (i - 50) / 50
-    print('Ratio = {}, paste = {}, shaker = {}\n'.format(ratio, N1, N2))
+    N1_cmp, N1_swap = pasteSort(data_curr1)
+    N2_cmp, N2_swap = shakeSort(data_curr2)
+    ratio = (i - 1000) / 1000
+    msg = 'Ratio = {}, paste: cmp {}, swap {}; shaker: cmp {}, swap {}\n'
+    print(msg.format(ratio, N1_cmp, N1_swap, N2_cmp, N2_swap))
